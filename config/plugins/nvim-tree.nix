af910acf4b5f4db2df47cc5daa9b80f47bd25b69
jan.mae <s8jamaeh@stud.uni-saarland.de>
2025-01-06 04:05:08 +0100

    Added nvim-tree keymaps

--- config/plugins/nvim-tree.nix
+++ config/plugins/nvim-tree.nix
@@ -5,11 +5,55 @@
 }:
 let
   cfg = config.modules.nvim-tree;
+  keymaps = [
+    {
+      mode = [ "n" ];
+      key = "<leader>ee";
+      action = "<cmd>NvimTreeToggle<CR>";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Toggle file explorer";
+      };
+    }
+    {
+      mode = [ "n" ];
+      key = "<leader>ef";
+      action = "<cmd>NvimTreeFindFileToggle<CR>";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Toggle file explorer on current file";
+      };
+    }
+    {
+      mode = [ "n" ];
+      key = "<leader>ec";
+      action = "<cmd>NvimTreeCollapse<CR>";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Collapse file explorer";
+      };
+    }
+    {
+      mode = [ "n" ];
+      key = "<leader>ec";
+      action = "<cmd>NvimTreeRefresh<CR>";
+      options = {
+        silent = true;
+        noremap = true;
+        desc = "Refresh file explorer";
+      };
+    }
+  ];
 in
 {
   options.modules.nvim-tree.enable = lib.mkEnableOption "Enables nvim-tree";
   
   config = lib.mkIf cfg.enable {
+    inherit keymaps;
+
     plugins.nvim-tree = {
       enable = true;
       autoReloadOnWrite = true;
